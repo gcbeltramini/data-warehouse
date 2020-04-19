ae930f33f145e13f4f7802d22e92c6ceebbeccc0
Guilherme Beltramini <guilherme.beltramini@nubank.com.br>
2020-04-18 22:29:09 -0300

    Refactor create_tables.py
    - Add comments
    - Add type annotations
    - Add docstrings
    - Simplify functions

--- create_tables.py
+++ create_tables.py
@@ -1,30 +1,56 @@
 import configparser
+from typing import List
+
 import psycopg2
+from psycopg2.extensions import connection, cursor
+
 from sql_queries import create_table_queries, drop_table_queries
 
 
-def drop_tables(cur, conn):
-    for query in drop_table_queries:
-        cur.execute(query)
+def execute_command(cur: cursor,
+                    conn: connection,
+                    commands: List[str]) -> None:
+    """
+    Execute commands in database.
+
+    Parameters
+    ----------
+    cur : psycopg2.extensions.cursor
+        Database cursor.
+    conn : psycopg2.extensions.connection
+        Database connection.
+    commands : list[str]
+        Commands or queries to be executed in the database.
+    """
+    for c in commands:
+        cur.execute(c)
         conn.commit()
 
 
-def create_tables(cur, conn):
-    for query in create_table_queries:
-        cur.execute(query)
-        conn.commit()
+def main() -> None:
+    """
+    Connect to database (parameters in file `dwh.cfg`), drop tables, create
+    tables and close the connection.
+    """
 
-
-def main():
+    # Read cluster configuration parameters
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
+    cluster_info = config['CLUSTER']
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    # Create a new database connection and cursor
+    conn = psycopg2.connect(host=cluster_info['HOST'],
+                            dbname=cluster_info['DB_NAME'],
+                            user=cluster_info['DB_USER'],
+                            password=cluster_info['DB_PASSWORD'],
+                            port=cluster_info['DB_PORT'])
     cur = conn.cursor()
 
-    drop_tables(cur, conn)
-    create_tables(cur, conn)
+    # Drop and create all tables
+    execute_command(cur, conn, commands=drop_table_queries)
+    execute_command(cur, conn, commands=create_table_queries)
 
+    # Close the connection
     conn.close()
 
 
