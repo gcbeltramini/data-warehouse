929a7db9f5a2d86d888cfaede4ce34725497a1f8
Guilherme Beltramini <guilherme.beltramini@nubank.com.br>
2020-04-18 23:12:13 -0300

    Use uppercase in constants

--- create_tables.py
+++ create_tables.py
@@ -1,12 +1,12 @@
 from db_utils import db_run
-from sql_queries import create_table_queries, drop_table_queries
+from sql_queries import CREATE_TABLE_QUERIES, DROP_TABLE_QUERIES
 
 
 def main() -> None:
     """
     Connect to database, drop and create tables, and close the connection.
     """
-    db_run(commands=drop_table_queries + create_table_queries)
+    db_run(commands=DROP_TABLE_QUERIES + CREATE_TABLE_QUERIES)
 
 
 if __name__ == "__main__":
--- etl.py
+++ etl.py
@@ -1,12 +1,12 @@
 from db_utils import db_run
-from sql_queries import copy_table_queries, insert_table_queries
+from sql_queries import COPY_TABLE_QUERIES, INSERT_TABLE_QUERIES
 
 
 def main() -> None:
     """
     Connect to database, insert data into tables, and close the connection.
     """
-    db_run(commands=copy_table_queries + insert_table_queries)
+    db_run(commands=COPY_TABLE_QUERIES + INSERT_TABLE_QUERIES)
 
 
 if __name__ == "__main__":
